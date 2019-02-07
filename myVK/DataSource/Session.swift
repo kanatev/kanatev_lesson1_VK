e25629f519ecd0b6083e7f42968ffa67a0f0e9b9
Aleksei Kanatev <aleksey.kanatev@mail.ru>
2019-02-07 16:24:12 +0500

    checking work of SourseTree

--- myVK/DataSource/Session.swift
+++ myVK/DataSource/Session.swift
@@ -11,6 +11,13 @@ import Foundation
 class Session {
 //    token: String – для хранения токена в VK,
 //    userId: Int – для хранения идентификатора пользователя ВК.
+    
+    //    token: String – для хранения токена в VK,
+    //    userId: Int – для хранения идентификатора пользователя ВК.
+    
+    //    token: String – для хранения токена в VK,
+    //    userId: Int – для хранения идентификатора пользователя ВК.
+    
     public static let sessionData = Session()
     
     var token: String = ""
